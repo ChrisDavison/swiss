c534bc4e833566cdc3e6580aaa80b33446068cf6
Chris Davison <c.jr.davison@gmail.com>
2024-09-16 13:40:37 +0100

    rust simple server

--- src/serve.rs
+++ src/serve.rs
@@ -1,20 +1,29 @@
-const template: &str = include_str!("template.html");
+use std::path::PathBuf;
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: &str, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+use rocket::fs::NamedFile;
+use rocket::response::content::RawHtml;
+use rocket::response::status::NotFound;
+use rocket::tokio::runtime;
+
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Result<NamedFile, NotFound<String>> {
+    let path = std::path::Path::new(&file);
+    NamedFile::open(&path)
+        .await
+        .map_err(|e| NotFound(e.to_string()))
 }
 
 #[get("/")]
-fn index() -> String {
-    template.to_string()
+pub fn index() -> RawHtml<String> {
+    RawHtml(std::fs::read_to_string("index.html").unwrap().to_string())
 }
 
-// #[launch]
-// fn rocket() -> _ {
-// }
-
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
-    rocket::build().mount("/", routes![hello, index]);
+    let _ = runtime::Runtime::new()?.block_on(async {
+        rocket::build()
+            .mount("/", routes![index, files])
+            .launch()
+            .await
+    });
     Ok(())
 }
